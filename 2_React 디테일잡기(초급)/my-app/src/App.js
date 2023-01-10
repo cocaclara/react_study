2cc7b3cfb03dfa5b814b47f20708e5b8df2fbd1c
Clara <m2nseo95@gmail.com>
2023-01-10 20:18:40 +0900

    리액트2-5강, 6강

--- 2_React 디테일잡기(초급)/my-app/src/App.js
+++ 2_React 디테일잡기(초급)/my-app/src/App.js
@@ -3,13 +3,17 @@ import './App.css';
 import Composition from './component/2-4.Props/Composition';
 import Extraction from './component/2-4.Props/Extraction/Extraction';
 import ClassComponent from './component/2-5.State/ClassComponent';
+import FunctionalComponent from './component/2-5.State/FunctionalComponent';
+import ClassComponent2 from './component/2-6.LifeCycle/ClassComponent';
 
 function App() {
   return (
     <div className="App">
-      <Composition />
+      {/* <Composition />
       <Extraction />
       <ClassComponent />
+      <FunctionalComponent /> */}
+      <ClassComponent2 />
     </div>
   );
 }
--- 2_React 디테일잡기(초급)/my-app/src/component/2-5.State/ClassComponent.jsx
+++ 2_React 디테일잡기(초급)/my-app/src/component/2-5.State/ClassComponent.jsx
@@ -7,10 +7,7 @@ export default class ClassComponent extends Component {
   }
 
   componentDidMount() {
-    this.timerID = setInterval(
-      () => this.tick(),
-      1000
-    );
+    this.timerID = setInterval(() => this.tick(),1000);
   }
 
   componentWillUnmount() {
@@ -26,7 +23,7 @@ export default class ClassComponent extends Component {
   render() {
     return (
       <div>
-        <h1>Hello, world!</h1>
+        <h1>Hello, world! It's Class Component!</h1>
         <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
       </div>
     );
